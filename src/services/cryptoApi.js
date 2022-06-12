bd9d338dccbd04abf9b5ceccdf9d311546ee3191
baolong3251 <baolong3251@gmail.com>
2022-06-12 23:42:04 +0700

    first commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const cryptoApiHeaders = {
+    // 'X-RapidAPI-Key': '30aefe1f7bmsh072f715abd8d959p1d505djsnc3c996f95cf6',
+    // 'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com"
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
